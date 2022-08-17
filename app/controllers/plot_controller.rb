a593096edf19f936854df93c2f6e6f745b7a6d79
Lucas-Jiang-9723 <yj1084@uchicago.edu>
2022-08-17 22:49:51 +0000

    8/17 5:49 commit - Playaround version

--- app/controllers/plot_controller.rb
+++ app/controllers/plot_controller.rb
@@ -25,4 +25,38 @@ class PlotController < ApplicationController
     end
     render({:template => "plot/argument.html.erb"})
   end
+ 
+  def sample_argument
+    @the_type = params.fetch("the_type")
+    
+    if @the_type=="Bar Chart"
+      @function = "sns.barplot"
+    elsif @the_type=="Box Plot"
+      @function = "sns.boxplot"
+    elsif @the_type=="Histogram"
+      @function = "sns.histplot"
+    elsif @the_type=="Line Chart"
+      @function = "sns.lineplot"
+    elsif @the_type=="Scatter Plot"
+      @function = "sns.scatterplot"
+    end
+    render({:template => "plot/sample_argument.html.erb"})
+  end
+
+  def play
+    render({:template => "plot/playaround.html.erb"})
+  end
+  def sample
+    render({:template => "plot/sample.html.erb"})
+  end
+  
+  def sample_input
+    @the_type = params.fetch("plotting_type")
+    redirect_to("/sample/choose_plotting_type/#{@the_type}")
+  end
+
+  def sample_the_type
+    @the_type = params.fetch("the_type")
+    render({:template => "plot/the_sample_type.html.erb"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,17 @@ Rails.application.routes.draw do
   #homepage
     get("/", {:controller=>"home", :action=>"index"})
   
+  #playaround
+    get("/playaround", {:controller=>"plot", :action=>"play"})
+  #sample data
+    get("/sample", {:controller=>"plot", :action=>"sample"})
+  #sample plotting type
+    get("/sample/choose_plotting_type",{:controller=>"plot", :action=>"sample_input"})  
+  #sample argument display
+  get("/sample/arguments/:the_type",{:controller=>"plot", :action=>"sample_argument"})
+  
+  #sample plotting type chosen
+    get("/sample/choose_plotting_type/:the_type", {:controller=>"plot", :action=>"sample_the_type"})
   #hesitate
     get("/hesitate", {:controller=>"home", :action=>"hesitate"})
   #choose plotting type
